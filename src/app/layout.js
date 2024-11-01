d7aefe3ec0287c85dc36692ed2fa8439ff4a10f7
u201923998 (Pacheco Alanya, Jesus Emerson) <jesuspacheco0219@gmail.com>
2024-10-31 23:08:33 -0500

    Fix: estilos en producciónsss

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,14 @@
-import './globals.css';
+import './globals.css'
 
 export const metadata = {
   title: 'Halloween Hack 2024',
   description: 'Una experiencia escalofriante te espera...',
-};
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es" suppressHydrationWarning>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <body className="bg-black min-h-screen" suppressHydrationWarning>
+    <html lang="es">
+      <body className="bg-black min-h-screen">
         {children}
       </body>
     </html>
